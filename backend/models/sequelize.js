7709a84fe6a7800b7bd62b89efb9ddf8803c64a7
Yosra Chebbi <yosrabouzouita16@gmail.com>
2021-12-21 03:00:05 +0100

    Add .env file

--- backend/models/sequelize.js
+++ backend/models/sequelize.js
@@ -3,9 +3,11 @@ var userModel = require("./User");
 var postsModel = require("./Post");
 var likeModel = require("./Like");
 var commentModel = require("./Comment");
+require('dotenv').config();
+const bcrypt = require("bcrypt");
 
-var database_model = new Sequelize("Groupomania", "root", "Yosrabouz@1986", {
-  host: "localhost",
+var database_model = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
   pool: {
     max: 10,
@@ -29,21 +31,23 @@ Post.hasMany(Comment);
 
 Comment.belongsTo(User);
 Post.belongsTo(User);
-//A faire decommenter une seule fois pour creer la BD puis commenter
-connectToDataBase(true);
+
+//A mettre dans .env CREATE_DB=true seule fois pour creer la BD puis mettre  à false
+connectToDataBase(process.env.CREATE_DB);
 
 async function connectToDataBase(is_first_connection) {
   if (is_first_connection) {
     database_model.sync({ force: true }).then(() => {
       console.log(`Database & tables created!`);
+      bcrypt.hash(process.env.ADMIN_PASSWORD, 10).then((hash) => {
       User.create({
-        firstname: "admin",
-        lastname: "admin",
-        email: "admin@groupomania.com",
-        password:
-          "$2b$10$SL6EbM8JsaVwG0B/n6WGj.jTKI5CK5BL2DdIoIzYBWk/G4QMOXhXG",
+        firstname: process.env.ADMIN_FIRSTNAME,
+        lastname: process.env.ADMIN_LASTNAME,
+        email: process.env.ADMIN_EMAIL,
+        password:hash,
         isAdmin: 1,
       });
+    });
     });
   } else {
     try {
